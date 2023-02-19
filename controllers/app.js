67eef4b906c0f7a17e8f9c9750e59d5483420af5
OlojaMuyiwa <olojam4969@gmail.com>
2023-02-19 02:35:50 +0100

    sorting

--- controllers/app.js
+++ controllers/app.js
@@ -1,12 +1,29 @@
 const Data = require("../models/App");
 
 const getAllData = async (req, res) => {
+  const { firstName, lastName, gender, height, weight, eyeColor } = req.query;
+  const queryOptions = {};
+
+  if (gender) {
+    queryOptions.gender = gender;
+  }
+  if (firstName) {
+    queryOptions.firstName = { $regex: firstName, $options: "i" };
+  }
+  if (lastName) {
+    queryOptions.lastName = { $regex: lastName, $options: "i" };
+  }
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
   try {
-    const data = await Data.find({}).limit(10);
+    const data = await Data.find(queryOptions).limit(limit);
     if (!data) {
       res.status(500).json({ msg: "An error occured", success: false });
     } else {
-      res.status(200).json({ data, success: true });
+      res.status(200).json({ data, success: true, total: data.length });
     }
   } catch (error) {
     res.status(500).json({ msg: error, success: false });
